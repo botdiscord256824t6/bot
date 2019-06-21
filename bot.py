3d3db5bcc2cbd6923de226412ba8cafbbdb12669
John Meow <mr.kofe.edk@ya.ru>
2019-06-21 17:48:30 +0300

    Better help

--- bot.py
+++ bot.py
@@ -77,35 +77,4 @@ async def info(ctx):
 
     await ctx.send(embed=embed)
 
-bot.remove_command('help')
-
-
-@bot.command()
-async def help(ctx):
-    embed = discord.Embed(title=botname, description="A" +
-                          botname + "." + " List of commands are:", color=0xeee657)
-
-    embed.add_field(
-        name="$plus X Y", value="Gives the addition of **X** and **Y**", inline=False)
-    embed.add_field(name="$umnozit X Y",
-                    value="Gives the multiplication of **X** and **Y**", inline=False)
-    embed.add_field(
-        name="$greet", value="Gives a nice greet message", inline=False)
-    embed.add_field(
-        name="$cat", value="Gives a cute cat gif to lighten up the mood.", inline=False)
-    embed.add_field(
-        name="$info", value="Gives a little info about the bot", inline=False)
-    embed.add_field(name="$help", value="Gives this message", inline=False)
-    await ctx.send(embed=embed)
-
-
-@bot.command()
-async def modhelp(ctx):
-    embed = discord.Embed(
-        title=botname, description='List of moderator command are:', color=0xeee657)
-    embed.add_field(name="%kick", value="Kick user", inline=False)
-    embed.add_field(name="%ban", value="Ban user", inline=False)
-    embed.add_field(name="%mute", value="Mute user", inline=False)
-    await ctx.send(embed=embed)
-
 bot.run(token)
